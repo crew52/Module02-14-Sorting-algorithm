fb6246a3871efdd254c213c1ac235322488aec14
crew52 <dungnc150602@gmail.com>
2024-12-24 14:24:48 +0700

    update all

---
+++ Sorting-algorithm/src/bt2/InsertionSortSteps.java
@@ -0,0 +1,72 @@
+package bt2;
+
+import java.util.Scanner;
+
+public class InsertionSortSteps {
+    public static void insertionSort(int[] list) {
+        int n = list.length;
+        for (int i = 1; i < n; i++) {
+            int key = list[i];
+            int j = i - 1;
+
+            // Di chuyển các phần tử lớn hơn key về phía sau
+            while (j >= 0 && list[j] > key) {
+                list[j + 1] = list[j];
+                j--;
+            }
+
+            list[j + 1] = key;
+        }
+    }
+
+    public static void insertionSortWithSteps(int[] arr) {
+        int n = arr.length;
+        System.out.println("Mảng ban đầu: ");
+        printArray(arr);
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Di chuyển các phần tử lớn hơn key về phía sau
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+
+            // Hiển thị mảng sau mỗi bước
+            System.out.println("Bước " + i + ": ");
+            printArray(arr);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập số lượng phần tử của mảng: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Nhập các phần tử của mảng: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("\n--- Bắt đầu thuật toán sắp xếp chèn ---\n");
+        insertionSortWithSteps(arr);
+
+        System.out.println("\n--- Mảng đã được sắp xếp ---");
+        printArray(arr);
+
+        scanner.close();
+    }
+}
